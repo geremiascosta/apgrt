af2c2eef2556b2ed90174df341587ae036864006
geremiascosta <62394868+geremiascosta@users.noreply.github.com>
2021-10-14 22:31:35 -0300

    Update dynamo-query.py

--- dynamo-query.py
+++ dynamo-query.py
@@ -31,3 +31,26 @@ def lambda_handler(event, context):
         'page': page,
         'size' : size
     }
+
+import json
+import boto3
+
+from decimal import Decimal
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('tb_transacao')
+        
+def lambda_handler(event, context):
+    id_transacao = "91e9b2e6-2e2a-4ec0-960a-a1610235f2cc"
+    response = table.get_item(Key={'id_transacao': id_transacao})
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response, cls=DecimalEncoder)
+    }
+
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
